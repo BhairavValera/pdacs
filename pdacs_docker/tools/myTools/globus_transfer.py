b4a88e1c13d6507869fb903f23a1a5324de39fa5
BhairavValera <bhairavvalera98@gmail.com>
2019-07-18 14:21:42 -0500

    Updated globus transfer

--- pdacs_docker/tools/myTools/globus_transfer.py
+++ pdacs_docker/tools/myTools/globus_transfer.py
@@ -23,7 +23,7 @@ source_endpoint_id = 'ffb116e6-d39e-11e7-9679-22000a8cbd7d'
 source_endpoint_path = '/Coyote/Grid/M001/L180/G001/'
 
 destination_endpoint_id = '3dce4b3c-24d0-11e9-9fa2-0a06afd4a22e'
-destination_endpoint_path = '/galaxy-central/database/files/' 
+destination_endpoint_path = '/cosmo/scratch/galaxy_ftp' 
 
 label = "Petrel to cosmo endpoint"
 
