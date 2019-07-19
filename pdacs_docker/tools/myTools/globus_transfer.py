854a0abb893779cd6e54d9081ed1738fac2162fb
BhairavValera <bhairavvalera98@gmail.com>
2019-07-19 13:32:33 -0500

    Latest update

--- pdacs_docker/tools/myTools/globus_transfer.py
+++ pdacs_docker/tools/myTools/globus_transfer.py
@@ -21,7 +21,7 @@ source_endpoint_id = 'ffb116e6-d39e-11e7-9679-22000a8cbd7d'
 source_endpoint_path = '/MiraTitanU/Grid/M000/L2100/HACC000/run/run000/'
 
 destination_endpoint_id = '3dce4b3c-24d0-11e9-9fa2-0a06afd4a22e'
-destination_endpoint_path = '/export/ftp/'
+destination_endpoint_path = '/cosmo/scratch/galaxy_ftp/'
 
 label = "Petrel to cosmo endpoint"
 
